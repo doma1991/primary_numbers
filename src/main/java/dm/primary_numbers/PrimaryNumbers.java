975a246e6c18cd9c0678baeb7a067b635dfef18f
doma91 <dominika.malinowska@sky.uk>
2019-10-06 15:37:37 +0100

    Splitting into 4 lists

--- src/main/java/dm/primary_numbers/PrimaryNumbers.java
+++ src/main/java/dm/primary_numbers/PrimaryNumbers.java
@@ -1,7 +1,11 @@
 package dm.primary_numbers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class PrimaryNumbers {
 
@@ -23,15 +27,64 @@ public class PrimaryNumbers {
     }
 
     public List<Integer> primes(int n) {
-    List<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+        List<Integer> numbers = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16);
+        List<Integer> numbers2 = numbers.subList(0, 4);
+        List<Integer> numbers3 = numbers.subList(4, 8);
+        List<Integer> numbers4 = numbers.subList(8, 12);
+        List<Integer> numbers5 = numbers.subList(12, 15);
 
-    int i = 0;
-    while (n > list.size()) {
-        i++;
-        if (isItPrime(i)) {
-            list.add(i);
-        }
-    }
+            ExecutorService executorService = Executors.newFixedThreadPool(4);
+            executorService.submit(() -> {
+                for (Integer number : numbers2) {
+                    if (isItPrime(number)) {
+                        list.add(number);
+                    }
+                }
+            });
+            executorService.submit(() -> {
+                for (Integer number1 : numbers3) {
+                    if (isItPrime(number1)) {
+                        list.add(number1);
+                    }
+                }
+            });
+            executorService.submit(() -> {
+                for (Integer number1 : numbers4) {
+                    if (isItPrime(number1)) {
+                        list.add(number1);
+                    }
+                }
+            });
+            executorService.submit(() -> {
+                for (Integer number1 : numbers5) {
+                    if (isItPrime(number1)) {
+                        list.add(number1);
+                    }
+                }
+            });
+
+//        List<Integer> x = new ArrayList<Integer>();
+//        x.add(1);
+//        x.subList(0, 4);
+//        ScheduledExecutorService executorService = Executors.newFixedThreadPool(4);
+//        for (int i = 0; i<4; i++) {
+//            executorService.submit(() -> {
+//                for (int number : mySublist) {
+//                    if (isItPrime(number)) {
+//                        list.add(number);
+//                    }
+//                }
+//            })
+//        }
+
+//        int i = 0;
+//    while (n > list.size()) {
+//        i++;
+//        if (isItPrime(i)) {
+//            list.add(i);
+//        }
+//    }
     return list;
     }
 }
